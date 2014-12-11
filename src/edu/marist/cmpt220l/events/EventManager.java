2a5025111b5e7fe182c295711f59ab11668c9e00
Steven Milanesi <sjmilanesi@users.noreply.github.com>
2014-12-11 14:53:58 -0500

    Update EventManager.java

--- src/edu/marist/cmpt220l/events/EventManager.java
+++ src/edu/marist/cmpt220l/events/EventManager.java
@@ -1,15 +1,31 @@
 package edu.marist.cmpt220l.events;
 
+import edu.marist.cmpt220l.teams.TeamManager;
+
 /**
  * The event manager manages the events in the application
  */
 public class EventManager {
     private Event[] events;
+    private Team[] t;
 
     /**
      * Construct a new EventManager
      */
     public EventManager() {
+        events = new Event[] {
+                new WashoosEvent(),
+                new HorseshoesEvent(),
+                new CornHoleEvent(),
+                new CanJamEvent(),
+                new LadderBallEvent(),
+                new StickGameEvent()
+        };
+    }
+    
+     public EventManager(TeamManager team) {
+        t = team.getTeams();
+        
         events = new Event[] {
                 new WashoosEvent(),
                 new HorseshoesEvent(),
@@ -28,4 +44,9 @@ public class EventManager {
     public Event[] getEvents() {
         return events;
     }
+    
+    public Event[] getTeams(){
+        return t;
+    }
+    
 }
